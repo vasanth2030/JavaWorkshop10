fb8e827ec614c13e9568d54eba314f52f2c49bb3
vasanth2030 <vasanths2030@gmail.com>
2023-03-07 21:33:57 +0530

    day1

---
+++ Java Code/basicjava/BasicJavaClass.java
@@ -0,0 +1,14 @@
+package basicjava;
+
+public class BasicJavaClass {
+	public static void main(String[] args) {
+		String name="Vasanth";
+		int age=21;
+		float height=(float)178.5;  //178.5f -> typecasting
+		
+		System.out.println("Hello World");
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Height: "+height+"cm");
+	}
+}
